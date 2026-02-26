a481600d6bf476c4341f5e54f76ed1b9a7cd51e3
Fabian Schneider <fa.schneider95@gmail.com>
2026-02-26 11:00:59 +0000

    feat: add catapult models (teams, athletes, activities, periods, parameters, workload)

--- src/shared/models/__init__.py
+++ src/shared/models/__init__.py
@@ -3,3 +3,11 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 from shared.models.user import User, UserRole  # noqa: E402, F401
+from shared.models.catapult import (  # noqa: E402, F401
+    CatapultActivity,
+    CatapultAthlete,
+    CatapultParameter,
+    CatapultPeriod,
+    CatapultTeam,
+    CatapultWorkload,
+)
